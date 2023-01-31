c4d3e3e2de1342fa713edc3cad1ff0d9cc2566a1
coolcjg <coolcjg@naver.com>
2023-01-31 22:10:29 +0900

    카프카 연동 테스트 확인

---
+++ pipe2/src/main/java/com/cjg/kafka/KafkaController.java
@@ -0,0 +1,30 @@
+package com.cjg.kafka;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+	
+	private final KafkaProducer producer;
+	
+	@Autowired
+	KafkaController(KafkaProducer producer){
+		this.producer = producer;
+	}
+	
+	
+	@PostMapping("/produce")
+	public String sendMessage(@RequestBody String message) {
+		this.producer.sendMessage(message);
+		return "success";
+	}
+	
+	
+
+}
